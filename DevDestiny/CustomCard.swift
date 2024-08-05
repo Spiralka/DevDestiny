adc9f495b0e837c679c6dde692caeea787520b0c
smetanka <spiralcka@gmail.com>
2024-08-06 01:01:45 +0300

    add cardView logic

--- DevDestiny/CustomCard.swift
+++ DevDestiny/CustomCard.swift
@@ -6,3 +6,36 @@
 //
 
 import Foundation
+import UIKit
+
+class CustomCard: SwipeCard {
+
+    private let imageView: UIImageView
+
+    init(image: UIImage) {
+        self.imageView = UIImageView(image: image)
+        super.init(frame: .zero)
+        setupCard()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupCard() {
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        addSubview(imageView)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: topAnchor),
+            imageView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            imageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+        
+        layer.cornerRadius = 6
+        layer.masksToBounds = true
+    }
+}
+
--- DevDestiny/ViewController.swift
+++ DevDestiny/ViewController.swift
@@ -6,13 +6,95 @@
 //
 
 import UIKit
+import Shuffle
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, SwipeCardStackDelegate, SwipeCardStackDataSource {
+  
+    @IBOutlet weak var doMagicButton: UIButton!
+    @IBOutlet weak var descriptionTextField: UITextView!
+    
+    @IBOutlet weak var doSomeMagicAgainButton: UIButton!
+    
+    @IBOutlet weak var effects: UIVisualEffectView!
+    
+    
+    private let cardStack = SwipeCardStack()
+    private var cardModels: [UIImage] = []
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        descriptionTextField.isHidden = true
+        loadCardModels()
+        cardStack.delegate = self
+        cardStack.dataSource = self
+        doSomeMagicAgainButton.isHidden = true
     }
+    
+
+    @IBAction func doSomeMagicAgainButtonTapped(_ sender: Any) {
+        cardStack.reloadData()
+        print("meow")
+        doMagicButton.isHidden = false
+        descriptionTextField.isHidden = true
+        doSomeMagicAgainButton.isHidden = true
+        
+    }
+    
+    @IBAction func doMagicTapped(_ sender: Any) {
+        doMagicButton.isHidden = true
+        view.addSubview(cardStack)
+        cardStack.frame = view.safeAreaLayoutGuide.layoutFrame
+    }
+    
+    
+    private func loadCardModels() {
+          if let image1 = UIImage(named: "card.png"),
+             let image2 = UIImage(named: "card.png"),
+             let image3 = UIImage(named: "card.png") {
+              cardModels = [image1, image2, image3]
+          }
+      }
+    
+    func cardStack(_ cardStack: SwipeCardStack, cardForIndexAt index: Int) -> SwipeCard {
+        let image = cardModels[index % cardModels.count]
+        let card = CustomCard(image: image)
+        return card
+
+    }
+    
+    func numberOfCards(in cardStack: SwipeCardStack) -> Int {
+        return cardModels.count 
+    }
+    
+    func getPrediction(){
+        descriptionTextField.text = """
+        Шут (The Fool):
+        Новый этап, неожиданные изменения. 
+        Будьте открыты для возможностей и рисков.
+
+        Маг (The Magician):
+        Все необходимые навыки и ресурсы у вас есть. 
+        Используйте их творчески и уверенно.
+
+        Верховная жрица (The High Priestess):
+        Прислушивайтесь к интуиции, 
+        анализируйте скрытые проблемы,
+        не торопитесь.
+
+        Итог:
+        Открытость к новому, уверенность в своих силах и интуитивное понимание помогут успешно провести релиз. Будьте готовы к неожиданностям и используйте свои ресурсы на полную мощность.
+"""
+    }
+    
+    func didSwipeAllCards(_ cardStack: SwipeCardStack) {
+            
+            getPrediction()
+            descriptionTextField.isHidden = false
+            doSomeMagicAgainButton.isHidden = false
+            cardStack.removeFromSuperview()
+        }
 
 
 }

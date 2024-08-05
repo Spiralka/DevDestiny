62cf3c6364853a34170292f9621c241c20e7ca0a
smetanka <spiralcka@gmail.com>
2024-08-06 00:52:11 +0300

    init

---
+++ DevDestiny/CustomCard.swift
@@ -0,0 +1,8 @@
+//
+//  CustomCard.swift
+//  DevDestiny
+//
+//  Created by Евгений Фомин on 24.06.2024.
+//
+
+import Foundation
---
+++ DevDestiny/Package.swift
@@ -0,0 +1,8 @@
+//
+//  Package.swift
+//  DevDestiny
+//
+//  Created by Евгений Фомин on 24.06.2024.
+//
+
+import Foundation
